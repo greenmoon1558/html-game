4838fba168dc212e3b9ca56c7428fcc17d1af1f0
CSJedi <tetiana.zhydeikina@gmail.com>
2020-04-17 21:00:40 +0300

    add app files with right structure

---
+++ CotoLearnerApp/ClientApp/components/SideBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="sidebar">
+    <h1 class="sidebar__title">{{ heading }}</h1>
+    <p>{{ headingText }}</p>
+    <p>{{ title }}</p>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+   data () {
+    return { 
+      heading: 'css catty',
+      headingText: 'Допоможи трьом котикам опинитися в їх будиночках.',
+      title: 'Позиція за замовчуванням',
+      text: 'За замовчуванням для елемента є позиція: статична, яка не впливає на елемент. Розглянемо наступні позиції, де всі коти сидять разом:'
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
